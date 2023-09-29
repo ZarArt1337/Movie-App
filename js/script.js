a24808ece5745a7d03ce7ad652e7bff4beb28f57
Artur Zaremba <rev133@gmail.com>
2023-09-29 13:20:00 +0200

    Added fetchByCategory, clear button, highlight categories. Removed video. Code cleared

--- js/script.js
+++ js/script.js
@@ -1,50 +1,60 @@
-import {categories} from "./categories.js";
+import { categories } from "./categories.js";
 
-const API_KEY = '32ccb349a74920e218cca3e62608f9ab';
-const API_URL = 'https://api.themoviedb.org/3/tv/top_rated?api_key=32ccb349a74920e218cca3e62608f9ab';
+const API_KEY = "32ccb349a74920e218cca3e62608f9ab";
+
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=32ccb349a74920e218cca3e62608f9ab&query=';
-const container = document.getElementById('container');
-const getCategory = document.getElementById('categories');
+
+const container = document.getElementById("container");
+const getCategory = document.getElementById("categories");
 const modal = document.getElementById("myModal");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+let selectedCategories = [];
 
-var lastUrl ='';
-
-
-
-//GETTING DATA
-// const fetchMovies = async () => {
-//     lastUrl = url;
-//     const res = await fetch(url);
-//     const json = await res.json();
-//     console.log(json.results);
-//     showMovies(json.results);
-//   };
-
-function fetchMovies(url) {
-    lastUrl = url;
-        fetch(url).then(res => res.json()).then(data => {
-            console.log(data.results)
-            showMovies(data.results);
-      })
+function fetchPopularTvSeries() {
+  fetchMovies(`https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}`);
 }
 
+function fetchPopularMovies() {
+  fetchMovies(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+}
+
+function fetchTopRatedMovies() {
+  fetchMovies(`https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`);
+}
+
+function fetchTopRatedTvSeries() {
+  fetchMovies(`https://api.themoviedb.org/3/tv/top_rated?api_key=${API_KEY}`);
+}
+
+function fetchMovies(url) {
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.results);
+      showMovies(data.results);
+    });
+}
 
 //DISPLAYING DATA
-const showMovies = (data) => {
-
-    data.forEach(movie => {
-        const {name, poster_path, vote_average, overview, id, first_air_date, title, release_date} = movie;
-        const movieEl = document.createElement('div');
-        movieEl.classList.add('movie');
-        movieEl.innerHTML = `
-             <img src="${poster_path? IMG_URL + poster_path : "http://via.placeholder.com/300x450"}" alt="${name}">
+const showMovies = data => {
+  container.innerHTML = "";
+  data.forEach(movie => {
+    const {name, poster_path, vote_average, overview, id, first_air_date, title, release_date} = movie;
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
+             <img src="${
+               poster_path
+                 ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                 : "http://via.placeholder.com/300x450"
+             }" alt="${name}">
 
             <div class="movie-info">
                 <h3>${name? name: title}</h3>
-                <span class="${getVoteColor(vote_average)}">${vote_average.toFixed(2)}</span>
+                <span class="${getVoteColor(
+                  vote_average
+                )}">${vote_average}</span>
             </div>
                 
             <div class="overview">
@@ -58,85 +68,140 @@ const showMovies = (data) => {
                 <br/> 
                 <button class="more" id="${id}">Read more</button>
             </div>
-        `
-        container.appendChild(movieEl);
+        `;
+    container.appendChild(movieEl);
 
-        document.getElementById(id).addEventListener('click', () => {
-            console.log(id);
-
-            fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`)
-                .then(response => response.json())
-                .then(response => console.log("response",`https://www.youtube.com/watch?v=${response.results[0].key}`));
-
-            openModal(movie);
-        })
-    })
-}
+    document.getElementById(id).addEventListener("click", () => {
+      console.log(id);
+      openModal(movie);
+    });
+  });
+};
 
 //OPENING & CLOSING MODAL
 const closeModal = () => {
-    modal.classList.remove("active");
+  modal.classList.remove("active");
 };
 
-const openModal = (movie) => {
+const generateModalHTML = movie => {
+  return `
+    <div class="modal_container">
+      <span class="close">&times;</span>
+      <h3>Movie name: ${movie.name? movie.name: movie.title}</h3>
+      <div class="modal_content">
+          <img src="${
+            movie.poster_path
+              ? IMG_URL + movie.poster_path
+              : "http://via.placeholder.com/300x450"
+          }" alt="${movie.name}" class="img_modal">
+          <div class="modal_other_info">
+              <p>Vote average: <span class="${getVoteColor(
+                movie.vote_average
+              )}">${movie.vote_average}</span></p>
+              <p>Release date: ${movie.first_air_date? movie.first_air_date: movie.release_date}</p>
+							<p>${movie.overview}</p>
+          </div>
+      </div>
+      
+    </div>`;
+};
 
-    modal.classList.add('active');
-    modal.innerHTML = `
-        <div class="modal_container">
-            <span class="close">&times;</span>
-            <h3>Movie name: ${movie.name? movie.name: movie.title}</h3>
-            <div class="modal_content">
-                <img src="${movie.poster_path? IMG_URL + movie.poster_path : "http://via.placeholder.com/300x450"}" alt="${movie.name}" class="img_modal">
-                <div class="modal_other_info">
-                    <span>Vote average: <span class="${getVoteColor(movie.vote_average)}">${movie.vote_average.toFixed(2)}</span></span>
-                    <span>First air date: ${movie.first_air_date? movie.first_air_date: movie.release_date}</span>
-                </div>
-            </div>
-            <p class="modal_overview">${movie.overview}</p>
-            <iframe src="https://www.youtube.com/watch?v=${movie.key}" frameborder="0"></iframe>
-        </div>`  
+const openModal = movie => {
+  modal.classList.add("active");
+  modal.innerHTML = generateModalHTML(movie);
 
-    document.addEventListener("click", function (event) {
-        if (event.target.classList.contains("close") ||
-          (event.target.classList.contains("active") &&
-            !event.target.closest(".modal_container"))
-        ) {
-          closeModal();
-        }
-    });         
-}
+  document.addEventListener("click", function (event) {
+    if (
+      event.target.classList.contains("close") ||
+      (event.target.classList.contains("active") &&
+        !event.target.closest(".modal_container"))
+    ) {
+      closeModal();
+    }
+  });
+};
 
 //GETTING VOTE AVERAGE COLOR
-const getVoteColor = (vote) => {
-    return vote >= 8 ? 'green' : vote >= 5 ? 'orange' : 'red';
-}
-  
-//DISPLAYING CATEGORY LIST
- const setCategories = () => {
-    categories.forEach(category => {
-        const categoryEl = document.createElement('div');
-        categoryEl.classList.add('category');
-        categoryEl.innerText = category.name;
-        getCategory.append(categoryEl);
-    })
-}
-
-
-fetchMovies(API_URL);
-setCategories();
-
+const getVoteColor = vote => {
+  return vote >= 8 ? "green" : vote >= 5 ? "orange" : "red";
+};
 
 //SEARCH MOVIES
-form.addEventListener('keyup', (e) => {
-    e.preventDefault();
-    
-    container.innerHTML = '';
+form.addEventListener("keyup", e => {
+  e.preventDefault();
 
-    const searchValue = search.value;
-    if(searchValue && searchValue !== '') {
-        fetchMovies(SEARCH_URL+searchValue);
-    }else{
-        fetchMovies(API_URL);
-    }
+  const searchValue = search.value;
 
+  if(searchValue) {
+      fetchMovies(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchValue}`);
+  } else {
+      fetchPopularMovies();
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchPopularMovies();
+});
+
+//DISPLAYING & FECTHING CATEGORIES
+function fetchByCategory() {
+	categories.forEach(category => {
+			const categoryEl = document.createElement('div');
+			categoryEl.classList.add('category');
+			categoryEl.id=category.id;
+			categoryEl.innerText = category.name;
+			categoryEl.addEventListener('click', () => {
+					if(selectedCategories.length == 0){
+							selectedCategories.push(category.id);
+					}else{
+							if(selectedCategories.includes(category.id)){
+									selectedCategories.forEach((id, index) => {
+											if(id == category.id){
+													selectedCategories.splice(index, 1);
+											}
+									})
+							}else{
+									selectedCategories.push(category.id);
+							}
+					}
+					console.log(selectedCategories)
+					fetchMovies('https://api.themoviedb.org/3/movie/popular?api_key=32ccb349a74920e218cca3e62608f9ab&with_genres='+selectedCategories)
+					highlightSelection();
+					
+			})
+			getCategory.append(categoryEl);
+	})
+}
+
+//HIGHLIGHTING CATEGORIES
+function highlightSelection() {
+	const tags = document.querySelectorAll('.category');
+	tags.forEach(tag => {
+			tag.classList.remove('highlight')
+			document.getElementById('clear_btn').classList.add('hide');
+	}) 
+	if(selectedCategories.length !=0){ 
+		document.getElementById('clear_btn').classList.remove('hide'); 
+		selectedCategories.forEach(id => {
+			const hightlightedCategory = document.getElementById(id);
+			hightlightedCategory.classList.add('highlight');
+		})
+	}
+}
+
+function removeHighlight() {
+	const clearCategory = document.querySelectorAll(".highlight");
+	clearCategory.forEach(clearHighlight => {
+		clearHighlight.classList.remove('highlight');
+	})
+}
+
+//CLEARING BUTTON
+document.getElementById("clear_btn").addEventListener("click", () => {
+	selectedCategories = [];
+	removeHighlight();
+	fetchPopularMovies();
+	document.getElementById('clear_btn').classList.add('hide');
 })
+
+fetchByCategory();
