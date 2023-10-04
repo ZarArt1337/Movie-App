40901a76581e56ecef520cd8cf0575f3ac81456e
Artur Zaremba <rev133@gmail.com>
2023-10-04 13:28:23 +0200

    Improved searching.

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,6 +2,10 @@ import { fetchMovies } from "../MovieList/MovieList.js";
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+const carousel = document.getElementById("carousel");
+const lists = document.getElementById("lists");
+const categories = document.getElementById('categories');
+const searchInfo = document.getElementById('search_info');
 
 form.addEventListener("keyup", e => {
   e.preventDefault();
@@ -11,7 +15,16 @@ form.addEventListener("keyup", e => {
   
   if(searchValue) {
     fetchMovies('search','movie', extraSearching);
+    carousel.classList.add('hide');
+    lists.classList.add('hide');
+    categories.classList.add('hide');
+    searchInfo.classList.add('show');
+    searchInfo.innerHTML = `<h1>Search reults for: '${searchValue}'</h1>`;
     } else {
       fetchMovies('movie','popular','');
+      carousel.classList.remove('hide');
+      lists.classList.remove('hide');
+      categories.classList.remove('hide');
+      searchInfo.classList.remove('show');
     }
 });
