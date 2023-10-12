82ec064dfa06c376f087f7ad1798ad6cb42b3b47
Artur Zaremba <rev133@gmail.com>
2023-10-12 15:00:45 +0200

    Fixed search

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,7 +2,6 @@ import { fetchMovies } from "../MovieList/MovieList.js";
 
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-const carousel = document.getElementById("carousel");
 const lists = document.getElementById("lists");
 const categories = document.getElementById('categories');
 const searchInfo = document.getElementById('search_info');
@@ -11,13 +10,12 @@ const boxFavourite = document.getElementById("favourite_box");
 const hideCategories = document.getElementById("show_categories");
 
 form.addEventListener("keyup", e => {
-  e.preventDefault();
+
   const searchValue = search.value;
   const extraSearching = `&query=${searchValue}`;
   
   if(searchValue) {
     fetchMovies('search','movie', extraSearching);
-    carousel.classList.add('hide');
     lists.classList.add('hide');
     categories.classList.add('hide');
     pagination.classList.add('hide');
@@ -27,13 +25,16 @@ form.addEventListener("keyup", e => {
     searchInfo.innerHTML = `<h1>Search reults for: '${searchValue}'</h1>`;
   } else {
     fetchMovies('movie','popular','');
-    carousel.classList.remove('hide');
     lists.classList.remove('hide');
     categories.classList.remove('hide');
     pagination.classList.remove('hide');
     searchInfo.classList.remove('show');
     hideCategories.classList.remove('hide');
   }
+});
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
 });
 
 
